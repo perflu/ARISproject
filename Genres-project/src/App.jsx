64d9e2be4b87195a0b0764cf83d2045458e921c7
rhkddms0908 <rhkddms0908@naver.com>
2025-06-04 11:24:33 +0900

    Add files via upload

---
+++ Genres-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GenresPage from './components/genrespage'
+import "./genrespage.css";
+
+function App() {
+  return (
+    <>
+    <GenresPage/>
+    </>
+  )
+}
+
+export default App
